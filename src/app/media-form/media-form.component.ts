109cf7551785d639f24430e833dfb93b717eb8e1
andrewalenta <info@datandre.de>
2024-03-23 22:46:00 +0100

    towards media archive

---
+++ src/app/media-form/media-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './media-form.component.html',
+  styleUrl: './media-form.component.css'
+})
+export class MediaFormComponent {
+
+}
